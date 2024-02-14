cc358d81f04656d6a55ca0b9af145b566956a295
Lucas2011 <31942492+Lucas2011@users.noreply.github.com>
2024-02-13 21:36:46 -0800

    opens the Notes app with the scanned tex

--- QRCodeCreator/ViewController.swift
+++ QRCodeCreator/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, AVCaptureMetadataOutputObjectsDelegate {
 
     var textField: UITextField!
     var qrCodeImageView: UIImageView!
     var generateButton: UIButton!
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func loadView() {
         super.loadView()
@@ -59,6 +61,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Set background color
         view.backgroundColor = .white
+        setupCamera()
+
     }
 
     // Called when the user presses the Return key
@@ -103,6 +107,89 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Generate QR Code and update ImageView
         generateQRCode()
     }
+    
+    func setupCamera() {
+          captureSession = AVCaptureSession()
+
+          guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+          let videoInput: AVCaptureDeviceInput
+
+          do {
+              videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+          } catch {
+              return
+          }
+
+          if (captureSession.canAddInput(videoInput)) {
+              captureSession.addInput(videoInput)
+          } else {
+              failed()
+              return
+          }
+
+          let metadataOutput = AVCaptureMetadataOutput()
+
+          if (captureSession.canAddOutput(metadataOutput)) {
+              captureSession.addOutput(metadataOutput)
+
+              metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+              metadataOutput.metadataObjectTypes = [.qr]
+          } else {
+              failed()
+              return
+          }
+
+          previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+          previewLayer.frame = view.layer.bounds
+          previewLayer.videoGravity = .resizeAspectFill
+          view.layer.addSublayer(previewLayer)
+
+          captureSession.startRunning()
+      }
+
+      func failed() {
+          let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+          ac.addAction(UIAlertAction(title: "OK", style: .default))
+          present(ac, animated: true)
+          captureSession = nil
+      }
+
+      override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+
+          if (captureSession?.isRunning == false) {
+              captureSession.startRunning()
+          }
+      }
+
+      override func viewWillDisappear(_ animated: Bool) {
+          super.viewWillDisappear(animated)
+
+          if (captureSession?.isRunning == true) {
+              captureSession.stopRunning()
+          }
+      }
+
+      func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+          captureSession.stopRunning()
+
+          if let metadataObject = metadataObjects.first {
+              guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+              guard let stringValue = readableObject.stringValue else { return }
+              AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+              found(code: stringValue)
+          }
+
+          dismiss(animated: true)
+      }
+
+      func found(code: String) {
+          // Open Notes app with scanned text
+          if let url = URL(string: "mobilenotes://"),
+             UIApplication.shared.canOpenURL(url) {
+              UIApplication.shared.open(url, options: [:], completionHandler: nil)
+          }
+      }
 }
 
 
